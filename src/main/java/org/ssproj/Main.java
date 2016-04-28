5b4e1f674174d8df90ff0c7ce55510c6079ad16d
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-04-28 18:12:51 +0900

    add traverse.

--- src/main/java/org/ssproj/Main.java
+++ src/main/java/org/ssproj/Main.java
@@ -19,11 +19,23 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class Main {
+public class Main implements Runnable {
     private final static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private final static AtomicBoolean done = new AtomicBoolean();
+    private final static Set<URL> check = Collections.synchronizedSet(new HashSet<>());
 
     public static void main(String[] args) throws IOException, ParseException {
         Options opts = new Options();
@@ -31,6 +43,8 @@ public class Main {
         opts.addOption("o", "output", true, "specify output directory. default is `screenshots`.");
         opts.addOption("i", "initial-sleep", true, "specify sleep in milliseconds before taking first screenshot. default is 5000.");
         opts.addOption("s", "sleep", true, "specify sleep in milliseconds before taking each screenshots. default is 500.");
+        opts.addOption("c", "concurrency", true, "specify concurrency. default is 2.");
+        opts.addOption("t", "traverse", false, "specify traverse other links. default is no traverse.");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cl = parser.parse(opts, args);
@@ -63,21 +77,88 @@ public class Main {
             sleep = 500;
         }
 
-        args = cl.getArgs();
-        final Main main = new Main(args[0], outputDirectory, browser, initialSleep, sleep);
-        main.run();
+        final int concurrency;
+        if (cl.hasOption('c')) {
+            concurrency = Integer.parseInt(cl.getOptionValue('c'));
+        } else {
+            concurrency = 2;
+        }
+
+        final boolean traverse;
+        if (cl.hasOption('t')) {
+            traverse = true;
+        } else {
+            traverse = false;
+        }
+
+        final BlockingQueue<URL> queue = new LinkedBlockingQueue<>();
+        final Thread[] threads = new Thread[concurrency];
+        for (int i = 0; i < concurrency; i++) {
+            final Runnable runner = new Main(queue, outputDirectory, browser, initialSleep, sleep, traverse);
+            threads[i] = new Thread(runner);
+            threads[i].start();
+        }
+
+        for (String fileName: cl.getArgs()) {
+            final LineIterator source;
+            try {
+                source = FileUtils.lineIterator(new File(fileName), "UTF-8");
+            } catch (IOException e) {
+                LOGGER.error("open error", e);
+                return;
+            }
+
+            try {
+                while (source.hasNext()) {
+                    final URL url;
+                    try {
+                        url = new URL(source.next());
+                    } catch (MalformedURLException e) {
+                        LOGGER.error("malformed error", e);
+                        continue;
+                    }
+
+                    queue.put(url);
+                }
+            } catch (InterruptedException e) {
+                LOGGER.error("interrupted", e);
+                break;
+            } finally {
+                source.close();
+            }
+        }
+
+        while (queue.size() > 0) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                LOGGER.error("interrupted", e);
+                break;
+            }
+        }
+
+        done.set(true);
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                LOGGER.error("interrupted", e);
+                break;
+            }
+        }
     }
 
-    private final String listFileName;
+    private final BlockingQueue<URL> queue;
     private final String outputDirectory;
     private final WebDriver driver;
     private final Wait<WebDriver> wait;
     private final long initialSleep;
     private final long sleep;
+    private final boolean traverse;
     private final AtomicLong counter = new AtomicLong();
 
-    public Main(String listFileName, String outputDirectory, String browser, long initialSleep, long sleep) {
-        this.listFileName = listFileName;
+    public Main(BlockingQueue<URL> queue, String outputDirectory, String browser, long initialSleep, long sleep, boolean traverse) {
+        this.queue = queue;
         this.outputDirectory = outputDirectory;
         if (browser.equalsIgnoreCase("firefox")) {
             System.out.println("activating firefox driver");
@@ -97,27 +178,25 @@ public class Main {
         this.wait = new WebDriverWait(this.driver, 30);
         this.initialSleep = initialSleep;
         this.sleep = sleep;
+        this.traverse = traverse;
     }
 
     public void run() {
-        final LineIterator source;
-        try {
-            source = FileUtils.lineIterator(new File(listFileName), "UTF-8");
-        } catch (IOException e) {
-            LOGGER.error("open error", e);
-            return;
-        }
-
-        while (source.hasNext()) {
+        while (!done.get()) {
             final URL url;
             try {
-                url = new URL(source.next());
-            } catch (MalformedURLException e) {
-                LOGGER.error("malformed error", e);
+                url = queue.poll(500, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                break;
+            }
+
+            if (url == null) {
                 continue;
             }
 
-            saveScreenShot(url);
+            if (check.add(url)) {
+                saveScreenShot(url);
+            }
         }
 
         this.driver.close();
@@ -149,6 +228,15 @@ public class Main {
             }
         } catch (IOException e) {
             LOGGER.error("save error", e);
+        }
+
+        // traverse
+        if (this.traverse) {
+            try {
+                traverse(url.toURI());
+            } catch (URISyntaxException e) {
+                LOGGER.error("syntax error", e);
+            }
         }
     }
 
@@ -209,5 +297,46 @@ public class Main {
         }
 
         return outputFile;
+    }
+
+    private void traverse(final URI baseUri) {
+        for (WebElement element : this.driver.findElements(By.tagName("a"))) {
+            try {
+                final String href = element.getAttribute("href");
+                if (href == null || href.isEmpty()) {
+                    continue;
+                }
+
+                URI uri = baseUri.resolve(href).normalize();
+                if (! baseUri.getHost().equalsIgnoreCase(uri.getHost())) {
+                    continue;
+                }
+
+                uri = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), uri.getPath(), uri.getQuery(), null);
+                URL newUrl = uri.toURL();
+                if (check.contains(newUrl)) {
+                    continue;
+                }
+
+                if (newUrl.getPath().startsWith("/mobile/")) {
+                    continue;
+                }
+                if (newUrl.getPath().startsWith("/.voice")) {
+                    continue;
+                }
+                if (newUrl.getPath().startsWith("/#/kana")) {
+                    continue;
+                }
+
+                this.queue.put(newUrl);
+            } catch (RuntimeException e) {
+                LOGGER.error("some error", e);
+            } catch (MalformedURLException | URISyntaxException e) {
+                LOGGER.error("malformed error", e);
+            } catch (InterruptedException e) {
+                LOGGER.error("interrupted", e);
+                break;
+            }
+        }
     }
 }
